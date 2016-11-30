021870a15d1bf3ecdbe8a31e2a8105935fbfd4a2
Ben Symonds <ben.symonds@opencorporates.com>
2016-11-30 16:08:28 +0000

    Allow setting of rollbar env with env variable
    We need this for distinguishing between production and staging (only review apps get `HEROKU_APP_NAME`). Only set environment if rollbar is enabled, to save us having to bother with configuring `ROLLBAR_ENV` in dev/test.

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,9 +1,11 @@
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
   config.enabled = false if Rails.env.test? || Rails.env.development?
-  config.environment = if ENV['HEROKU_APP_NAME'].present?
-    ENV['HEROKU_APP_NAME'][/(?<=-)pr-\d+$/]
-  else
-    Rails.env
+  if config.enabled
+    config.environment = if ENV['HEROKU_APP_NAME'].present?
+      ENV['HEROKU_APP_NAME'][/(?<=-)pr-\d+$/]
+    else
+      ENV.fetch('ROLLBAR_ENV')
+    end
   end
 end
