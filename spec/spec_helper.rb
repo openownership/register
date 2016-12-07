2b429a821d2564b5fb4b28bcd1256ffaa1d8e603
Tim Craft <mail@timcraft.com>
2016-12-07 14:06:48 +0000

    Remove config.profile_examples setting to disable rspec profiling

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,7 +14,6 @@ RSpec.configure do |config|
   config.example_status_persistence_file_path = "spec/examples.txt"
   config.disable_monkey_patching!
   config.default_formatter = 'doc' if config.files_to_run.one?
-  config.profile_examples = 10
 
   config.order = :random
   Kernel.srand config.seed
