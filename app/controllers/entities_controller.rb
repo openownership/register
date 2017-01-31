1ff6dd6a7e551b7e8d286057ffde23cd9465f973
Tim Craft <mail@timcraft.com>
2017-01-31 11:02:10 +0000

    Display ultimate source relationships on entity pages

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -4,6 +4,6 @@ class EntitiesController < ApplicationController
 
     @source_relationships = Relationship.where(source: @entity)
 
-    @target_relationships = Relationship.where(target: @entity)
+    @ultimate_source_relationship_groupings = RelationshipGraph.new(@entity).ultimate_source_relationships.group_by { |r| r.source.name }.sort
   end
 end
