f14b8291f83bb36e6eefd63d8568fb35e5bcedb7
Tim Craft <mail@timcraft.com>
2017-01-31 11:02:10 +0000

    Add RelationshipGraph class

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -5,4 +5,7 @@ class Relationship
 
   belongs_to :source, class_name: 'Entity'
   belongs_to :target, class_name: 'Entity'
+
+  embeds_many :intermediate_entities, class_name: 'Entity'
+  embeds_many :intermediate_relationships, class_name: 'Relationship'
 end
