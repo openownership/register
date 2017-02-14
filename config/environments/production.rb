6094673f6083fc7d8f7b9387bbc28b2f5f3dc045
Ben Symonds <ben.symonds@opencorporates.com>
2017-02-14 15:18:20 +0000

    Force SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,6 +12,7 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
+  config.force_ssl = true
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger = ActiveSupport::Logger.new(STDOUT)
