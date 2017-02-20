5a2a7dc90b7794cbc0095d1db193c5bb04682385
Ben Symonds <ben.symonds@opencorporates.com>
2017-02-20 10:16:40 +0000

    Immediately load source relationships for entity page
    As it stands it is a `Mongoid::Criteria`s, which means each time the records are accessed (eg. in the view we do `empty?` and then `sort_by`) they will hit the database.

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -2,7 +2,7 @@ class EntitiesController < ApplicationController
   def show
     @entity = Entity.find(params[:id])
 
-    @source_relationships = Relationship.where(source: @entity)
+    @source_relationships = Relationship.where(source: @entity).entries
 
     @ultimate_source_relationship_groupings = RelationshipGraph.new(@entity).ultimate_source_relationships.group_by { |r| r.source.name }.sort
   end
