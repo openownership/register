4f83b3abae8ffce714cfbf5214f306d3ecde9ecc
Ben Symonds <ben.symonds@opencorporates.com>
2017-02-23 09:37:14 +0000

    Add more detail to entity "titles"

--- app/helpers/entity_helper.rb
+++ app/helpers/entity_helper.rb
@@ -1,8 +1,13 @@
 module EntityHelper
-  def entity_jurisdiction(entity)
+  def entity_jurisdiction(entity, short: false)
     return unless (country = entity.country)
-    return country.name unless (country_subdivision = entity.country_subdivision)
-    "#{country_subdivision.name} (#{country.name})"
+
+    if entity.country_subdivision
+      country_label = short ? country.alpha2 : country.name
+      "#{entity.country_subdivision.name} (#{country_label})"
+    else
+      short ? country.names[0] : country.name
+    end
   end
 
   def entity_country_flag(entity)
@@ -13,5 +18,27 @@ module EntityHelper
     return unless asset_present?(basename)
 
     image_tag(basename, size: '32x16', alt: country.name, class: 'flag')
+  end
+
+  def entity_attributes_snippet(entity)
+    parts = []
+    if entity.natural_person?
+      parts << entity.country.try(:nationality)
+      date_of_birth(entity).presence.try do |date_of_birth|
+        parts << "(Born #{date_of_birth})"
+      end
+    else
+      parts << entity_jurisdiction(entity, short: true)
+      parts << "(#{entity.incorporation_date} – #{entity.dissolution_date})" if entity.incorporation_date?
+    end
+    parts.compact.join(' ')
+  end
+
+  def date_of_birth(entity)
+    parts = []
+    parts << entity.dob_day if entity.dob_month?
+    parts << Date::MONTHNAMES[entity.dob_month] if entity.dob_month? && (entity.dob_day? || entity.dob_year?)
+    parts << entity.dob_year
+    parts.compact.join(" ")
   end
 end
--- spec/helpers/entity_helper_spec.rb
+++ spec/helpers/entity_helper_spec.rb
@@ -26,7 +26,8 @@ RSpec.describe EntityHelper do
   end
 
   describe '#entity_jurisdiction' do
-    subject { helper.entity_jurisdiction(entity) }
+    subject { helper.entity_jurisdiction(entity, short: short) }
+    let(:short) { false }
 
     context 'when the entity has a country' do
       context 'when the entity does not have a subdivision' do
@@ -35,6 +36,14 @@ RSpec.describe EntityHelper do
 
         it 'returns the name of the country' do
           expect(subject).to eq('United Kingdom of Great Britain and Northern Ireland')
+        end
+
+        context "when short requested" do
+          let(:short) { true }
+
+          it "returns the shorter name of the country" do
+            expect(subject).to eq('United Kingdom')
+          end
         end
       end
 
@@ -45,6 +54,14 @@ RSpec.describe EntityHelper do
         it 'returns the name of the subdivision' do
           expect(subject).to eq('Delaware (United States of America)')
         end
+
+        context "when short requested" do
+          let(:short) { true }
+
+          it "returns the shorter name of the country" do
+            expect(subject).to eq('Delaware (US)')
+          end
+        end
       end
     end
 
@@ -53,6 +70,88 @@ RSpec.describe EntityHelper do
 
       it 'returns nil' do
         expect(subject).to be_nil
+      end
+    end
+  end
+
+  describe '#entity_attributes_snippet' do
+    subject { helper.entity_attributes_snippet(entity) }
+
+    context "when entity is a natural person" do
+      before { entity.type = Entity::Types::NATURAL_PERSON }
+
+      context "when entity doesn't have attributes" do
+        before do
+          allow(helper).to receive(:date_of_birth).with(entity).and_return(nil)
+          allow(entity).to receive(:country).and_return(nil)
+        end
+
+        it "returns nothing" do
+          expect(subject).to be_blank
+        end
+      end
+
+      context "when entity has attributes" do
+        before do
+          allow(helper).to receive(:date_of_birth).with(entity).and_return("February 1980")
+          allow(entity).to receive(:country).and_return(ISO3166::Country[:GB])
+        end
+
+        it "returns formatted string" do
+          expect(subject).to eq("British (Born February 1980)")
+        end
+      end
+    end
+
+    context "when entity is a legal entity" do
+      before { entity.type = Entity::Types::LEGAL_ENTITY }
+
+      context "when entity doesn't have attributes" do
+        before do
+          allow(helper).to receive(:entity_jurisdiction).with(entity, short: true).and_return(nil)
+          entity.incorporation_date = entity.dissolution_date = nil
+        end
+
+        it "returns nothing" do
+          expect(subject).to be_blank
+        end
+      end
+
+      context "when entity has attributes" do
+        before do
+          allow(helper).to receive(:entity_jurisdiction).with(entity, short: true).and_return("United Kingdom")
+          entity.incorporation_date = entity.dissolution_date = Date.new(1980, 2, 27)
+        end
+
+        it "returns formatted string" do
+          expect(subject).to eq("United Kingdom (1980-02-27 – 1980-02-27)")
+        end
+      end
+    end
+  end
+
+  describe '#date_of_birth' do
+    subject { helper.date_of_birth(entity) }
+
+    {
+      [1980, nil, nil] => "1980",
+      [nil,  2,   nil] => "",
+      [nil,  nil, 27] => "",
+      [1980, 2,   nil] => "February 1980",
+      [1980, nil, 27] => "1980",
+      [nil,  2,   27] => "27 February",
+      [1980, 2,   27] => "27 February 1980"
+    }.each do |(year, month, day), expected|
+      context "when entity data of birth data is #{[year, month, day]}" do
+        before do
+          entity.dob_year = year
+          entity.dob_month = month
+          entity.dob_day = day
+        end
+
+        it "returns #{expected}" do
+          expect(subject).to eq(expected)
+        end
       end
     end
   end
