6af11d466c15c9cfca36ca18cfb4465e0c62009b
Ben Symonds <ben.symonds@opencorporates.com>
2017-02-27 09:40:11 +0000

    Refactor spec so that method under test is `subject`

--- spec/helpers/entity_helper_spec.rb
+++ spec/helpers/entity_helper_spec.rb
@@ -2,59 +2,61 @@ require 'rails_helper'
 
 RSpec.describe EntityHelper do
   describe '#entity_country_flag' do
+    subject { helper.entity_country_flag(entity) }
+
     context 'when the entity has a jurisdiction code' do
-      subject { Entity.new(jurisdiction_code: 'gb') }
+      let(:entity) { Entity.new(jurisdiction_code: 'gb') }
 
       it 'returns the corresponding country flag image' do
-        image = helper.entity_country_flag(subject)
-
-        expect(image).to match(/^<img /)
-        expect(image).to match(%r{src="/assets/GB-.+\.svg"})
-        expect(image).to match(/alt="United Kingdom/)
+        expect(subject).to match(/^<img /)
+        expect(subject).to match(%r{src="/assets/GB-.+\.svg"})
+        expect(subject).to match(/alt="United Kingdom/)
       end
     end
 
     context 'when the entity does not have a jurisdiction code' do
-      subject { Entity.new }
+      let(:entity) { Entity.new }
 
       it 'returns nil' do
-        expect(helper.entity_country_flag(subject)).to be_nil
+        expect(subject).to be_nil
       end
     end
 
     context 'when the entity has an unknown jurisdiction code' do
-      subject { Entity.new(jurisdiction_code: 'xxx') }
+      let(:entity) { Entity.new(jurisdiction_code: 'xxx') }
 
       it 'returns nil' do
-        expect(helper.entity_country_flag(subject)).to be_nil
+        expect(subject).to be_nil
       end
     end
   end
 
   describe '#entity_jurisdiction' do
+    subject { helper.entity_jurisdiction(entity) }
+
     context 'when the entity has a jurisdiction code' do
       context 'when the jurisdiction matches a country' do
-        subject { Entity.new(jurisdiction_code: 'gb') }
+        let(:entity) { Entity.new(jurisdiction_code: 'gb') }
 
         it 'returns the name of the country' do
-          expect(helper.entity_jurisdiction(subject)).to eq('United Kingdom of Great Britain and Northern Ireland')
+          expect(subject).to eq('United Kingdom of Great Britain and Northern Ireland')
         end
       end
 
       context 'when the jurisdiction matches a subdivision of a country' do
-        subject { Entity.new(jurisdiction_code: 'us_de') }
+        let(:entity) { Entity.new(jurisdiction_code: 'us_de') }
 
         it 'returns the name of the subdivision' do
-          expect(helper.entity_jurisdiction(subject)).to eq('Delaware (United States of America)')
+          expect(subject).to eq('Delaware (United States of America)')
         end
       end
     end
 
     context 'when the entity does not have a jurisdiction code' do
-      subject { Entity.new }
+      let(:entity) { Entity.new }
 
       it 'returns nil' do
-        expect(helper.entity_jurisdiction(subject)).to be_nil
+        expect(subject).to be_nil
       end
     end
   end
