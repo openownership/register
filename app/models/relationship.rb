c027111461861f5ef2b1c96f77c75bedebb82ee4
Tim Craft <mail@timcraft.com>
2017-03-13 13:06:00 +0000

    Add Provenance model

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -6,6 +6,7 @@ class Relationship
   belongs_to :source, class_name: 'Entity'
   belongs_to :target, class_name: 'Entity'
 
+  embeds_one :provenance
   embeds_many :intermediate_entities, class_name: 'Entity'
   embeds_many :intermediate_relationships, class_name: 'Relationship'
 
