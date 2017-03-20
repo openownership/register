6e435c4721111fa0cabfa48896658397dd2906ed
Ben Symonds <ben.symonds@opencorporates.com>
2017-03-20 13:27:10 +0000

    Raise email delivery failures in development
    To get dev/prod parity. Can use `mailcatcher` as the local SMTP server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,6 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
-  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_caching = false
   config.active_support.deprecation = :log
   config.assets.debug = true
