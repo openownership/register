9ed2b60af4ebf79f09fd4dc06fc0908c740d0b7f
Ben Symonds <ben.symonds@opencorporates.com>
2017-03-20 20:34:37 +0000

    Get rid of devise bits we're not using

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,13 @@ class User
   include Mongoid::Document
 
   devise :database_authenticatable, :registerable,
-         :confirmable, :recoverable, :rememberable, :trackable, :validatable
+         :confirmable, :recoverable, :trackable, :validatable
 
   field :email,              type: String, default: ""
   field :encrypted_password, type: String, default: ""
 
   field :reset_password_token,   type: String
   field :reset_password_sent_at, type: Time
-
-  field :remember_created_at, type: Time
 
   field :sign_in_count,      type: Integer, default: 0
   field :current_sign_in_at, type: Time
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,6 @@ Devise.setup do |config|
   config.skip_session_storage = [:http_auth]
   config.stretches = Rails.env.test? ? 1 : 11
   config.reconfirmable = true
-  config.expire_all_remember_me_on_sign_out = true
   config.password_length = 6..128
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
