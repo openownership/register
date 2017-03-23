faa9ab323f50f4fad33c1cf310f4beb08426ab83
Ben Symonds <ben.symonds@opencorporates.com>
2017-03-23 12:32:06 +0000

    Allow static assets to be served without basic auth
    So that Gmail's image caching can work (with basic auth the Gmail servers cannot access the image to cache it).

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ Bundler.require(*Rails.groups)
 module OpenOwnershipRegister
   class Application < Rails::Application
     if ENV["BASIC_AUTH"].present?
-      config.middleware.insert_before(Rack::Sendfile, Rack::Auth::Basic) do |u, p|
+      config.middleware.insert_after(ActionDispatch::Static, Rack::Auth::Basic) do |u, p|
         [u, p] == ENV["BASIC_AUTH"].split(':')
       end
     end
