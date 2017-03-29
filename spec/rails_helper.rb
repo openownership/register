f50bd94d5083cc2768eaf92f86356419548ee599
Thomas Marshall <thomas.marshall@opencorporates.com>
2017-03-29 16:06:34 +0100

    Add user factory

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/factory_girl'
 
 Dir["./spec/shared_examples/**/*.rb"].each { |f| require f }
 
