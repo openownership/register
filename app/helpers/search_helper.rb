236234e79d13046322ac9df91b7b075608644242
Ben Symonds <ben.symonds@opencorporates.com>
2017-04-02 22:39:39 +0100

    Add tooltips for "legal entity"

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,14 +1,16 @@
 module SearchHelper
   def search_filter_type(value)
-    icon_filename = if value == Entity::Types::NATURAL_PERSON
-      'icon-natural-person.svg'
+    if value == Entity::Types::NATURAL_PERSON
+      icon_filename = 'icon-natural-person.svg'
+      label = t(value, scope: :entity_types).capitalize
     else
-      'icon-legal-entity.svg'
+      icon_filename = 'icon-legal-entity.svg'
+      label = glossary_tooltip(t(value, scope: :entity_types).capitalize, value, :right)
     end
 
     icon = content_tag(:span, image_tag(icon_filename), class: 'filter-type-icon')
 
-    [t(value, scope: :entity_types).capitalize, icon]
+    [label, icon]
   end
 
   def search_filter_country(code)
