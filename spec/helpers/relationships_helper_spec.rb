d4c159829c1d853e662b53d2d85f6139204cf0fe
Thomas Marshall <thomas.marshall@opencorporates.com>
2017-04-07 14:17:27 +0100

    Group "undefined" interests together
    Some relationships have multiple "undefined" interests, e.g.
    "significant-influence-or-control" and
    "significant-influence-or-control-as-firm". They should only display
    once, rather than doubling up.

--- spec/helpers/relationships_helper_spec.rb
+++ spec/helpers/relationships_helper_spec.rb
@@ -1,25 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe RelationshipsHelper do
-  describe '#format_interest' do
-    it 'uses the translation' do
-      expect(helper.format_interest('ownership-of-shares-25-to-50-percent')).to eq(I18n.t('relationship_interests.ownership-of-shares-25-to-50-percent'))
+  let(:relationship) { instance_double('Relationship') }
+  let(:known_interests) { ['Ownership of shares - 20.0%', 'ownership-of-shares-25-to-50-percent'] }
+  let(:unknown_interests) { ['significant-influence-or-control-as-trust', 'some-other-interest'] }
+
+  before do
+    allow(relationship).to receive(:interests).and_return(known_interests + unknown_interests)
+  end
+
+  describe '#known_interests_for' do
+    it 'only includes known interests' do
+      expect(helper.known_interests_for(relationship)).to eq(known_interests)
     end
+  end
 
-    it 'falls back to a default' do
-      expect(helper.format_interest('hello-world', 'fallback')).to eq('fallback')
-    end
-
-    it 'returns interests from submissions' do
-      a = I18n.t('submissions.relationships.interests.ownership_of_shares_percentage', value: 20.5)
-      b = I18n.t('submissions.relationships.interests.voting_rights_percentage', value: 12)
-      c = I18n.t('submissions.relationships.interests.right_to_appoint_and_remove_directors')
-      d = I18n.t('submissions.relationships.interests.other_significant_influence_or_control', value: 'Hello world')
-
-      expect(helper.format_interest(a)).to eq(a)
-      expect(helper.format_interest(b)).to eq(b)
-      expect(helper.format_interest(c)).to eq(c)
-      expect(helper.format_interest(d)).to eq(d)
+  describe '#unknown_interests_for' do
+    it 'only includes unknown interests' do
+      expect(helper.unknown_interests_for(relationship)).to eq(unknown_interests)
     end
   end
 end
