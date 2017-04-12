780422499ee25a8d5e0b1d21897ce6b767eed723
Thomas Marshall <thomas.marshall@opencorporates.com>
2017-04-12 15:45:35 +0100

    Use ISO8601::Date for `sample_date` field on relationship

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -4,7 +4,7 @@ class Relationship
   field :_id, type: Hash
 
   field :interests, type: Array, default: []
-  field :sample_date, type: String
+  field :sample_date, type: ISO8601::Date
 
   belongs_to :source, class_name: 'Entity', inverse_of: :relationships_as_source
   belongs_to :target, class_name: 'Entity', inverse_of: :_relationships_as_target
