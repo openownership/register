8673878d6abcead582ba0134af5925e41e4b2908
James Darling <james@abscond.org>
2017-06-14 14:52:06 +0100

    Force elasticsearch index after seeding
    Without doing this, new installers following the install guide will get
    a indexing error message when trying to search.

--- spec/features/seeds_spec.rb
+++ spec/features/seeds_spec.rb
@@ -5,7 +5,16 @@ RSpec.feature "seeds" do
     File.join(Rails.root, 'db', 'seeds.rb')
   end
 
+  before do
+    allow(Entity).to receive(:import)
+  end
+
   it "runs without error" do
     expect { load(file) }.not_to raise_error
+  end
+
+  it "imports entities to elasticsearch" do
+    load(file)
+    expect(Entity).to have_received(:import).with(force: true)
   end
 end
