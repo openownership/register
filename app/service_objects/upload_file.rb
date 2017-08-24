4ec0710e9d5da73d790248fd1dea15d9040ef8ab
Phil Thompson <phil@electricvisions.com>
2017-08-24 09:56:22 +0100

    Add PR number to export path

--- app/service_objects/upload_file.rb
+++ app/service_objects/upload_file.rb
@@ -1,14 +1,14 @@
 class UploadFile
-  def self.from(file, to:, s3: Aws::S3::Resource.new)
-    new(s3).from(file, to: to)
+  def self.from(file, to:, s3: Aws::S3::Resource.new, pr: PullRequestNumber.new)
+    new(s3).from(file, to: to, pr: pr)
   end
 
   def initialize(s3)
     @bucket = s3.bucket(ENV['BUCKETEER_BUCKET_NAME'])
   end
 
-  def from(file, to:)
-    @bucket.object(File.join('public', to))
+  def from(file, to:, pr:)
+    @bucket.object(['public', pr.call, to].compact.join('/'))
       .upload_file(file.path)
   end
 end
