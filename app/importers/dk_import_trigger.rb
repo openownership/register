2c1ead9b383d8335f41aa3fc8af4579d28fe1cd4
Steve Day <steve@openownership.org>
2018-12-11 11:27:22 +0000

    Lazily loop over chunks of results from DK api
    If we don't do this lazily, the memory usage grows and grows, resulting
    (at last import) in nearly 2G of memory used. I think this is because,
    whilst we enumerate over the results in pages, we're still building up
    a full array of results in all_records. Profiling the loop with the
    memory_profiler gem showed retained objects from the json gem (and only
    that gem). These objects disappear when we add .lazy.

--- app/importers/dk_import_trigger.rb
+++ app/importers/dk_import_trigger.rb
@@ -5,7 +5,7 @@ class DkImportTrigger
       Rails.application.config.dk_cvr.password,
     )
     retreived_at = Time.zone.now.to_s
-    client.all_records.each_slice(chunk_size) do |records|
+    client.all_records.lazy.each_slice(chunk_size) do |records|
       # records is an array of hashes, but our ChunkHelper works with
       # arrays of strings
       strings = records.map(&:to_json)
