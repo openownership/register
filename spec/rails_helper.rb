38d03647b8ee5ff100ad6b434b761d5f0a80e738
Steve Day <steve@openownership.org>
2019-01-03 17:30:32 +0000

    Configure Capybara to use headless chrome for js tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,7 @@ require File.expand_path('../config/environment', __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'support/capybara'
 require 'support/devise'
 require 'support/submission_helpers'
 require 'support/admin_helpers'
