73978d6be1ab9761fe6740997b928b2dba5b5bd4
Steve Day <steve@openownership.org>
2019-01-14 14:56:19 +0000

    Add system tests for searching
    Basic happy path tests for the info shown on the page and the functions
    like pagination etc.

---
+++ spec/system/searches/pagination_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe 'Search pagination' do
+  let!(:companies) { FactoryGirl.create_list(:legal_entity, 20) }
+  let!(:people) { FactoryGirl.create_list(:natural_person, 20) }
+
+  before do
+    Entity.import(force: true, refresh: true)
+  end
+
+  it 'can page through results' do
+    visit '/'
+    within '.search-content' do
+      fill_in 'q', with: 'Example' # Matches all people and companies
+      click_button 'Search'
+    end
+
+    expect(page).to have_selector('.list-entities .item', count: 10)
+    expect(page).to have_text 'Displaying results 1 - 10 of 40 in total'
+
+    within '.pagination' do
+      within '.current' do
+        expect(page).to have_text('1')
+      end
+
+      expect(page).to have_link '2'
+      expect(page).to have_link '3'
+      expect(page).to have_link '4'
+      expect(page).to have_link 'Next ›'
+      expect(page).to have_link 'Last »'
+
+      click_link '3'
+    end
+
+    within '.pagination' do
+      expect(page).to have_link '« First'
+      expect(page).to have_link '‹ Prev'
+      expect(page).to have_link '1'
+      expect(page).to have_link '2'
+      within '.current' do
+        expect(page).to have_text('3')
+      end
+      expect(page).to have_link '4'
+      expect(page).to have_link 'Next ›'
+      expect(page).to have_link 'Last »'
+
+      click_link 'Last »'
+    end
+
+    within '.pagination' do
+      expect(page).to have_link '« First'
+      expect(page).to have_link '‹ Prev'
+      expect(page).to have_link '1'
+      expect(page).to have_link '2'
+      expect(page).to have_link '3'
+      within '.current' do
+        expect(page).to have_text('4')
+      end
+    end
+  end
+end
