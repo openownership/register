70179acd7cbc0d6de75fd47e735cc0f86c0f7d5c
Steve Day <steve@openownership.org>
2019-02-06 15:59:54 +0000

    Add user agent blocking to Rack::Attack config
    We tried being nice and blocking some bots via robots.txt, but not all
    of them respect it, so this ups the ante and blocks them completely by
    their user agent.
    
    Following the existing convention for blocking ip addresses, I've used
    an env var we can then tweak in production, with the idea that initially
    at least this will contain simple strings similar to those you'd put in
    the robots.txt. They're matched case-insensitively and anywhere in the
    UA string.

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,5 @@
 Rails.application.config.blocked_ips = ENV.fetch('BLOCKED_IPS', '').split(',').map(&:strip)
+Rails.application.config.blocked_uas = ENV.fetch('BLOCKED_USER_AGENTS', '').split(',').map(&:strip)
 
 X_FORWARDED_FOR_HEADER = 'HTTP_X_FORWARDED_FOR'.freeze
 
@@ -8,5 +9,13 @@ Rack::Attack.blocklist("blocked IPs") do |request|
     ips.split(',').map(&:strip).map(&:presence).compact.any? do |ip|
       Rails.application.config.blocked_ips.include?(ip)
     end
+  end
+end
+
+Rack::Attack.blocklist("blocked user agents") do |request|
+  ua = request.user_agent.strip
+  return false if ua.blank?
+  Rails.application.config.blocked_uas.any? do |blocked_ua|
+    ua.match(Regexp.new(blocked_ua, Regexp::IGNORECASE))
   end
 end
