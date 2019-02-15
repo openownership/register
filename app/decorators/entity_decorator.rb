4662a28ac8dce2877ecbc2acd53faf653e1f165a
Steve Day <steve@openownership.org>
2019-02-15 16:43:20 +0000

    Output a fixed string when a company name is blank
    Because we already use Draper decorators, this seemed like the best
    place to put the logic (because it's only a presentational concern).
    
    There's a slight bit of complication because we already define a name
    method on the decorator to deal with transliteration, hence why I alias
    it before overriding.
    
    Fixes OO-371

--- app/decorators/entity_decorator.rb
+++ app/decorators/entity_decorator.rb
@@ -4,4 +4,13 @@ class EntityDecorator < ApplicationDecorator
   decorates_finders
 
   transliterated_attrs :name, :address, :company_type
+
+  alias transliterated_name name
+  def name
+    if object.name.blank?
+      I18n.t 'entities.show.company_name_missing'
+    else
+      transliterated_name
+    end
+  end
 end
