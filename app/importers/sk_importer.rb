225311ba07cce15c7a901b474ca8025bb5f7033d
Steve Day <steve@openownership.org>
2019-02-19 11:50:38 +0000

    Move Slovakian address check into child_entity! method
    It makes more sense to have all of the 'validity' checks for child
    entities in the same place and logged in a similar way so that we can
    keep tabs on them.
    
    This also has the side effect of checking the address on the *current*
    child entity, not just the first, which seems much more correct.

--- app/importers/sk_importer.rb
+++ app/importers/sk_importer.rb
@@ -10,10 +10,7 @@ class SkImporter
   end
 
   def process(record)
-    return unless slovakian_address?(record['PartneriVerejnehoSektora'].first['Adresa'])
-
     child_entity = child_entity!(record)
-
     return if child_entity.nil?
 
     record['KonecniUzivateliaVyhod'].each do |item|
@@ -28,9 +25,11 @@ class SkImporter
   def child_entity!(record)
     item = record['PartneriVerejnehoSektora'].find { |p| p['PlatnostDo'].nil? }
 
-    # See OO-251
     if item.nil?
       Rails.logger.warn("[#{self.class.name}] record Id: #{record['Id']} has no current child entity (PartneriVerejnehoSektora)")
+      return
+    elsif !slovakian_address?(item['Adresa'])
+      Rails.logger.warn("[#{self.class.name}] record Id: #{record['Id']} has a child entity (PartneriVerejnehoSektora) with a non-Slovakian address")
       return
     elsif item['ObchodneMeno'].nil?
       Rails.logger.warn("[#{self.class.name}] record Id: #{record['Id']} has a child entity (PartneriVerejnehoSektora) with no company name (ObchodneMeno)")
