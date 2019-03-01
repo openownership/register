9e45fae92593596e8c08dc4ad05c8a22fef6da0d
Steve Day <steve@openownership.org>
2019-03-01 15:59:19 +0000

    Paginate owned companies in EntitiesController#show
    This adjusts the querying/decorating for source relationships so that we
    can paginate them with Kaminari and present a paginated list to the
    user.
    
    Previously, these relationships were accessed via
    entity.relationships_as_source, sorted by a RelationshipsSorter and then
    sorted again in the view by name.
    
    This had several problems:
    - Using relationships_as_source and then accessing the target entities
      for each relationship results in an N+1 query situation, where each
      entity has to be queried separately to retrieve its details.
    - RelationshipsSorter sorted by ended_date, but the view then sorted by
      name, resulting in ended relationships no longer appearing at the
      bottom and a waste of effort in the initial sorting.
    
    Instead, this changes the query for source relationships to use
    Relationship.includes instead, so that all related companies are
    returned in one query, which is considerable performance improvement.
    
    In addition, RelationshipsSorter now sorts by ended_date *and* name,
    which I think was the original intention, which means we don't need to
    sort anything in the view.
    
    The sorting is less than ideal, because it means we cannot use
    pagination to reduce the number of Relationships we have to get from the
    db (MongoDB cannot sort across 'related' collections like a relational
    database could). However, since the current numbers (1000s in the worst
    case but not 10,000s) don't seem to be *too* slow, this is the
    least-worst option to get the UX we want. The time taken to render the
    resulting entities is a much bigger chunk of the total processing time.
    
    The pagination is copied directly from the search page, so that the UX
    is familiar to the user.

--- spec/system/entities/show_spec.rb
+++ spec/system/entities/show_spec.rb
@@ -28,12 +28,81 @@ RSpec.describe 'Viewing an entity' do
     expect(page).to have_text 'British'
     expect(page).to have_text person.nationality
     expect(page).to have_text birth_month_year(person)
-    expect(page).to have_text "Companies controlled by #{person.name} #{company.name}"
+    expect(page).to have_text company.name
     expect(page).to have_link 'Google'
     expect(page).to have_link 'Report incorrect data'
 
     expect(page).to have_text company.incorporation_date.iso8601
     expect(page).to have_link relationship_link(relationship), href: relationship_href(relationship)
     expect(page).to have_text ownership_summary(relationship)
+  end
+
+  context 'when a company owns lots of others' do
+    let!(:relationships) do
+      FactoryGirl.create_list(
+        :relationship,
+        10,
+        source: company,
+        interests: ['ownership-of-shares-25-to-50-percent'],
+      )
+    end
+
+    let!(:ended_relationship_1) do
+      FactoryGirl.create(
+        :relationship,
+        source: company,
+        interests: ['ownership-of-shares-25-to-50-percent'],
+        ended_date: Time.zone.yesterday.iso8601,
+      )
+    end
+
+    let!(:ended_relationship_2) do
+      FactoryGirl.create(
+        :relationship,
+        source: company,
+        interests: ['ownership-of-shares-25-to-50-percent'],
+        ended_date: Time.zone.today.iso8601,
+      )
+    end
+
+    let(:ended_company_1) { ended_relationship_1.target }
+    let(:ended_company_2) { ended_relationship_2.target }
+
+    it 'paginates the list of owned companies' do
+      visit entity_path(company)
+      expect(page).to have_text 'Displaying companies 1 - 10 of 12 in total'
+
+      within '.pagination' do
+        within '.current' do
+          expect(page).to have_text('1')
+        end
+        expect(page).to have_link '2'
+        expect(page).to have_link 'Next ›'
+        expect(page).to have_link 'Last »'
+        click_link '2'
+      end
+
+      within '.pagination' do
+        expect(page).to have_link '« First'
+        expect(page).to have_link '‹ Prev'
+        expect(page).to have_link '1'
+        within '.current' do
+          expect(page).to have_text('2')
+        end
+      end
+    end
+
+    it "puts ended relationships at the end" do
+      visit entity_path(company)
+      # Page 1
+      expect(page).not_to have_text(ended_company_1.name)
+      expect(page).not_to have_text(ended_company_2.name)
+
+      within('.pagination') { click_link '2' }
+
+      # Page 2
+      expect(page).to have_text(ended_company_1.name)
+      expect(page).to have_text(ended_company_2.name)
+    end
   end
 end
