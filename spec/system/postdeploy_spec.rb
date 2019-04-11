7053cdfe1aa37e57a8b4931ada0a96208b92eb1d
Steve Day <steve@openownership.org>
2019-04-11 15:47:32 +0100

    Move postdeploy spec setup inside example
    I think it needs to be here to properly clear the DB etc, without it we
    get duplicate errors for User emails (when there aren't any dupes in the
    data).

--- spec/system/postdeploy_spec.rb
+++ spec/system/postdeploy_spec.rb
@@ -1,14 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe 'postdeploy task' do
-  before do
+  it 'loads the example data' do
     OpenOwnershipRegister::Application.load_tasks
     Rake::Task['postdeploy'].invoke
-    # Force ES to index the documents right now (normally it waits 1 second)
-    Entity.__elasticsearch__.refresh_index!
-  end
 
-  it 'loads the example data' do
     expect(Entity.count).to eq(4341)
     expect(Relationship.count).to eq(3100)
     expect(Submissions::Submission.count).to eq(9)
