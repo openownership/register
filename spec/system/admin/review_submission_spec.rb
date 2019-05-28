bf0c2102e8e244256f006e43601e9011c670697f
Steve Day <steve@openownership.org>
2019-05-28 15:51:55 +0200

    Send a notification email when submissions are submitted
    Sends an email to the addresses defined in ADMIN_EMAILS. Whilst testing
    this I also refactored the tests of the other submission emails slightly
    so that they used capybara-email's capabilities.
    
    For OO-143

--- spec/system/admin/review_submission_spec.rb
+++ spec/system/admin/review_submission_spec.rb
@@ -10,14 +10,24 @@ RSpec.describe 'review submission' do
     stub_elasticsearch
     stub_opencorporates_client_get_company
     stub_opencorporates_client_search_companies
+    SubmissionMailer.submission_approval_requested(submission).deliver_now
   end
 
-  scenario 'approve' do
+  scenario 'approve through admin dashboard' do
     visit admin_submissions_path
     click_on submission.entity.name
     click_on approve_button
+    open_email(submission.user.email)
+    expect(current_email.subject).to eq(submission_approved_subject)
+  end
+
+  scenario 'approve through email notification' do
+    open_email admin_email
+    current_email.click_link approve_link
+    click_on approve_button
     expect(page).to have_text(success_notice)
-    expect(last_delivered_email.subject).to eq(submission_approved_subject)
+    open_email(submission.user.email)
+    expect(current_email.subject).to eq(submission_approved_subject)
   end
 
   def approve_button
@@ -28,11 +38,15 @@ RSpec.describe 'review submission' do
     I18n.t('admin.submissions.approve.success', entity: submission.entity.name)
   end
 
-  def last_delivered_email
-    ActionMailer::Base.deliveries.last
-  end
-
   def submission_approved_subject
     I18n.t('submission_mailer.submission_approved.subject')
+  end
+
+  def admin_email
+    ENV['ADMIN_EMAILS'].split(',').first
+  end
+
+  def approve_link
+    I18n.t('submission_mailer.submission_approval_requested.view')
   end
 end
