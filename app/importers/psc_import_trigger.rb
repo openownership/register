6d878818629ec21ced9ca53812b1c833995fc3b6
Steve Day <steve@openownership.org>
2019-06-14 12:33:32 +0200

    Update PscImportTrigger to get config from DataSource
    Since we already store all the info we need in the DataSource, it makes
    sense to just load it rather than hard code it in this class, and this
    is a step towards storing records of the import itself too.
    
    I added a spec to test the regex parsing in this class as well since it
    was untested before.

--- app/importers/psc_import_trigger.rb
+++ app/importers/psc_import_trigger.rb
@@ -1,29 +1,30 @@
 require 'open-uri'
 
 class PscImportTrigger
-  BASE_URL = 'http://download.companieshouse.gov.uk'.freeze
-  DOWNLOAD_PAGE = "#{BASE_URL}/en_pscdata.html".freeze
-
   FILENAME_REGEX = /^psc-snapshot-\d{4}-\d{2}-\d{2}_\d+of\d+.zip$/
 
-  def call(chunk_size)
-    snapshot_links = parse_snapshot_links
+  def call(data_source, chunk_size)
+    data_source_uri = URI.parse(data_source.url)
+    base_url = "#{data_source_uri.scheme}://#{data_source_uri.host}"
+    snapshot_links = parse_snapshot_links(data_source.url, base_url)
 
-    raise "No PSC snapshot links found on #{BASE_URL}" if snapshot_links.blank?
+    raise "No PSC snapshot links found on #{base_url}" if snapshot_links.blank?
 
     Rails.logger.info "Scheduling PSC import jobs for the following snapshot links: #{snapshot_links}"
 
-    snapshot_links.each { |l| PscFileProcessorWorker.perform_async(l, chunk_size) }
+    snapshot_links.each do |link|
+      PscFileProcessorWorker.perform_async(link, chunk_size)
+    end
   end
 
   private
 
-  def parse_snapshot_links
-    document = Nokogiri::HTML(open(DOWNLOAD_PAGE).read)
+  def parse_snapshot_links(download_page, base_url)
+    document = Nokogiri::HTML(open(download_page).read)
     document.css('a').each_with_object([]) do |el, acc|
       href = el['href']
       if href.present? && href.match(FILENAME_REGEX)
-        acc << "#{BASE_URL}/#{href}"
+        acc << "#{base_url}/#{href}"
       end
     end
   end
