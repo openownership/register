f1d489dea4cb0c4513651e20046b0f3060caa45c
Steve Day <steve@openownership.org>
2019-08-14 16:37:22 +0100

    Add factories for OOR/UA/EITI DataSources
    For OO-242

--- spec/factories/data_sources.rb
+++ spec/factories/data_sources.rb
@@ -22,5 +22,25 @@ FactoryGirl.define do
       name 'Denmark Central Business Register (Centrale Virksomhedsregister [CVR])'
       document_id 'Denmark CVR'
     end
+
+    factory :ua_data_source do
+      name 'Ukraine Consolidated State Registry (Edinyy Derzhavnyj Reestr [EDR])'
+      url 'https://data.gov.ua/dataset/1c7f3815-3259-45e0-bdf1-64dca07ddc10'
+      document_id 'Ukraine EDR'
+    end
+
+    factory :eiti_data_source do
+      name 'EITI Pilot Data - Example'
+      url 'https://docs.google.com/spreadsheets/d/1OKl6oe6RbYicPIZEGljYZy29M06Pm4vGvGDoNzq6dV4'
+      document_id 'EITI Structured Data - Example'
+      types %w[thirdParty primaryResearch]
+    end
+
+    factory :oor_data_source do
+      name 'OpenOwnership Register'
+      url 'https://register.openownership.org'
+      document_id nil
+      types %w[thirdParty selfDeclaration]
+    end
   end
 end
