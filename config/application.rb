943e61e91c5728e2ca6e3e473e17c4ade75cbecb
Steve Day <steve@openownership.org>
2019-11-27 13:15:45 +0000

    Set default_url_options in application.rb
    So that we can use url helpers in service objects and other random
    classes that might need them. Note I've copied the mailer settings
    because we don't send mails with links to different domains so far.

--- config/application.rb
+++ config/application.rb
@@ -41,6 +41,8 @@ module OpenOwnershipRegister
     config.action_mailer.default_url_options[:port] = host_uri.port if host_uri.port != host_uri.default_port
     config.action_mailer.asset_host = host_uri.to_s
 
+    self.default_url_options = config.action_mailer.default_url_options
+
     config.skylight.probes += %w[redis mongoid faraday]
   end
 end
