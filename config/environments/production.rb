d3fa4561f3f35a5d728ec1ca44996c6c8888c3e0
Steve Day <steve@openownership.org>
2020-01-06 09:57:01 +0000

    Bump rails to 5.2.4.1 and associated gems
    A change to I18n's default locale setting is all that's needed for
    compatibility.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,7 +10,7 @@ Rails.application.configure do
   config.log_level = :info
   config.log_tags = [:request_id]
   config.action_mailer.perform_caching = false
-  config.i18n.fallbacks = true
+  config.i18n.fallbacks = [I18n.default_locale]
   config.active_support.deprecation = :notify
   config.log_formatter = ::Logger::Formatter.new
   config.force_ssl = true
