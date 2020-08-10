58ddd417069c6f7d330d21543d76edc06e3d71b9
Steve Day <steve@openownership.org>
2020-08-10 11:28:08 +0100

    Don't collect any personal data with Rollbar
    To save us having to explain and get consent for it, we can just tell
    Rollbar to anonymise IP addresses, skip emails and only collect our
    internal user ids so that they don't have any personal data.

--- config/initializers/rollbar.rb
+++ config/initializers/rollbar.rb
@@ -1,5 +1,11 @@
 Rollbar.configure do |config|
   config.access_token = ENV['ROLLBAR_ACCESS_TOKEN']
+  config.anonymize_user_ip = true
+  config.scrub_fields << [:user_email]
+  # Only collect ids, no personal data
+  config.person_username_method = ''
+  config.person_email_method = ''
+
   config.enabled = false if Rails.env.test? || Rails.env.development?
   if config.enabled
     config.environment = (PullRequestNumber.call || ENV.fetch('ROLLBAR_ENV'))
