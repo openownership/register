d171b033c88503c94a77f81a2b4b742d4d1ffa28
Steve Day <steve@openownership.org>
2020-08-18 11:12:09 +0100

    Swap ReCAPTCHA for invisible_captcha
    This is a more privacy-preserving service which we don't need to get
    explicit consent for, deal with cookies, etc.

---
+++ config/initializers/invisible_captcha.rb
@@ -0,0 +1,6 @@
+InvisibleCaptcha.setup do |config|
+  config.honeypots = Rails.cache.fetch('invisible_captcha_honeypots') do
+    (1..20).map { InvisibleCaptcha.generate_random_honeypot }
+  end
+  config.timestamp_enabled = !Rails.env.test?
+end
