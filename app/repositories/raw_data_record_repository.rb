112649a26908c61c07f2e2d82db5296a7f1ddea5
Josh Williams <josh@spacesnottabs.com>
2023-06-12 12:22:22 +0000

    Updated raw data repository for PSC fixes with source url

--- app/repositories/raw_data_record_repository.rb
+++ app/repositories/raw_data_record_repository.rb
@@ -1,17 +1,43 @@
 require 'ostruct'
 require 'register_sources_psc/repositories/company_record_repository'
+require 'register_sources_bods/structs/identifier'
 
 class RawDataRecordRepository
   def initialize
     @repository = RegisterSourcesPsc::Repositories::CompanyRecordRepository.new
   end
 
-  def all_for_entity(entity)
-    bods_identifiers = entity.identifiers # identifier_converter.convert_v1_to_v2 entity.identifiers
+  def all_for_entity(main_entity)
+    return all_for_entity(main_entity.master_entity) if main_entity.master_entity
+
+    bods_identifiers = []
+
+    [
+      main_entity,
+      main_entity.relationships_as_source,
+      main_entity.relationships_as_target
+    ].flatten.map(&:bods_statement).each do |bods_statement|
+      if bods_statement.respond_to?(:identifiers)
+        bods_identifiers += bods_statement.identifiers
+      end
+
+      if bods_statement.respond_to?(:source)
+        source = bods_statement.source
+        if source
+          s = source.url.to_s.split("https://api.company-information.service.gov.uk")
+          if s.length > 1
+            bods_identifiers += [RegisterSourcesBods::Identifier.new(
+              schemeName: 'GB Persons Of Significant Control Register',
+              id: s[1]
+            )]
+          end
+        end
+      end
+    end
 
     return [] if bods_identifiers.empty?
 
-    get_by_bods_identifiers(bods_identifiers) # .order_by(updated_at: :desc, created_at: :desc)
+    get_by_bods_identifiers(bods_identifiers.uniq) # .order_by(updated_at: :desc, created_at: :desc)
   end
 
   def newest_for_entity(entity)
