51b242a8833bee348002ab89db9b2aa027c184cc
Nicolas Williams <tp@tiredpixel.com>
2023-10-04 15:13:44 +0200

    [#188] upgrade register-*

--- config/initializers/adapters.rb
+++ config/initializers/adapters.rb
@@ -2,7 +2,7 @@ require 'register_common/adapters/s3_adapter'
 require 'register_common/structs/aws_credentials'
 
 Rails.application.config.s3_adapter = RegisterCommon::Adapters::S3Adapter.new(
-  credentials: RegisterCommon::Structs::AwsCredentials.new(
+  credentials: RegisterCommon::AwsCredentials.new(
     ENV.fetch('BODS_EXPORT_AWS_REGION', 'eu-west-1'),
     ENV.fetch('BODS_EXPORT_AWS_ACCESS_KEY_ID'),
     ENV.fetch('BODS_EXPORT_AWS_SECRET_ACCESS_KEY')
