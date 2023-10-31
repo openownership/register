openownership/register

commit 530222f7b6d586750ff5a3a8db61fe6da3c5e470

[#188] refactor misc config/

config/routes.rb
@@ -1,22 +1,26 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'pages#home'
+
+  resources :data_sources, only: %i[index show]
+
   resources :entities, only: :show do
+    resources :relationships, only: :show, path: ''
     member do
       get 'graph'
-      get 'raw'
       get 'opencorporates-additional-info'
+      get 'raw'
     end
-    resources :relationships, only: :show, path: ''
   end
+
   resource :search, only: :show
-  get 'terms-and-conditions' => 'pages#terms_and_conditions'
-  get 'privacy' => 'pages#privacy'
-  get 'faq' => 'pages#faq'
-  get 'glossary' => 'pages#glossary'
-  get 'download' => 'pages#download'
-  get 'download/latest' => 'pages#download_latest'
-  get 'data-changelog' => 'pages#data_changelog'
-  resources :data_sources, only: %i[index show]
-  root 'pages#home'
+
+  get 'data-changelog',       to: 'pages#data_changelog'
+  get 'download',             to: 'pages#download'
+  get 'download/latest',      to: 'pages#download_latest'
+  get 'faq',                  to: 'pages#faq'
+  get 'glossary',             to: 'pages#glossary'
+  get 'privacy',              to: 'pages#privacy'
+  get 'terms-and-conditions', to: 'pages#terms_and_conditions'
 end

config/environments/development.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
-  # Enable 10.0.2.2 for development to enable using NAT port forwarding through a VM
-  config.web_console.permissions = '10.0.2.2'
-
   config.cache_classes = false
   config.eager_load = false
   config.consider_all_requests_local = true

config/initializers/iso8601.rb
@@ -1,30 +1,0 @@
-# frozen_string_literal: true
-
-require 'iso8601'
-
-module ISO8601
-  class Date
-    def mongoize
-      @original
-    end
-
-    def self.mongoize(object)
-      case object
-      when Date
-        object.mongoize
-      when String
-        Date.new(object).mongoize
-      when NilClass
-        nil
-      else
-        raise "Unable to mongoize #{object.class.name} #{object} as #{name}"
-      end
-    end
-
-    def self.demongoize(string)
-      return if string.blank?
-
-      new(string)
-    end
-  end
-end

commit dc59a1eea17613d1ac0ac6419609261d1e4be26e

[#188] fix graph routing
This was broken during the refactor in
530222f7b6d586750ff5a3a8db61fe6da3c5e470 .

config/routes.rb
@@ -6,12 +6,12 @@ Rails.application.routes.draw do
   resources :data_sources, only: %i[index show]
 
   resources :entities, only: :show do
-    resources :relationships, only: :show, path: ''
     member do
       get 'graph'
       get 'opencorporates-additional-info'
       get 'raw'
     end
+    resources :relationships, only: :show, path: ''
   end
 
   resource :search, only: :show