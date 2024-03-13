c812ac5507dbe13f74ef3708da7ea09d4abe3d0a
Nicolas Williams <tp@tiredpixel.com>
2024-03-13 14:09:04 +0000

    [#216] update for repositories refactor

--- app/repositories/entity_service.rb
+++ app/repositories/entity_service.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
-require 'register_sources_bods/repositories/bods_statement_repository'
 require 'register_sources_bods/register/entity_service'
+require 'register_sources_bods/repository'
 
 class EntityService < RegisterSourcesBods::Register::EntityService
   def initialize
-    repository = RegisterSourcesBods::Repositories::BodsStatementRepository.new
+    repository = RegisterSourcesBods::Repository.new(index: RegisterSourcesBods::Config::ELASTICSEARCH_INDEX)
     super(statement_repository: repository)
   end
 end
